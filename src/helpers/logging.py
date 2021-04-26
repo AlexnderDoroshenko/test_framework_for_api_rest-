19bde65aab6fd9d8d585145fba4de590d5076710
AlexnderDoroshenko <doroshenkoaldm@gmail.com>
2021-04-26 10:07:18 +0300

    Refactor logger

--- src/helpers/logging.py
+++ src/helpers/logging.py
@@ -1,13 +1,13 @@
 import logging
+import settings
 
 
 class Logger:
     def __init__(self):
         self.log = logging
-        self.file_name = "api.log"
-        self.basic = self.log.basicConfig(filename='app.log',
-                                          filemode='w',
-                                          format='%(name)s - %(levelname)s - %(message)s')
+        self.file_name = f"{settings.ROOT_DIR}/api.log"
+        self.basic = self.log.basicConfig
+        self.basic(filename=self.file_name, filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
     def set_log_format(self, format_str: str):
         self.basic(format=format_str)
